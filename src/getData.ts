7ddcaca32d95d06631c8d768c1a542036546a8fb
Vanessa Mitelman <vanessamitelman@gmail.com>
2023-07-11 19:51:13 +0300

    moved client.ts to get data

--- src/getData.ts
+++ src/getData.ts
@@ -1,6 +1,20 @@
 import { createClient } from 'contentful';
-import { contentTypeObj, clientObj } from './client';
+// import { contentTypeObj, clientObj } from './client';
 
+interface ClientI {
+  space: string;
+  accessToken: string;
+}
+interface ContentTypeI {
+  content_type: string;
+}
+export const clientObj: ClientI = {
+  space: 'arw5tmqsoy9e',
+  accessToken: '8rJFvhxfbfqsNT6H48NKSjq64AcBd5tnopzvSNsSNcE'
+};
+export const contentTypeObj: ContentTypeI = {
+  content_type: 'cmcCategories'
+};
 const client = createClient(clientObj);
 
 const getData = async () => {
